4c5905a7c8cf45f7be9984678b304dd52f1e8141
Aleksi Martikainen <aleksi.martikainen@gofore.com>
2023-10-02 02:22:45 +0300

    cleaning up

--- scripts/radio.gd
+++ scripts/radio.gd
@@ -1,15 +1,29 @@
-extends RigidBody3D
+extends Interactable
 
-var has_electricity = false
+class_name radio
+
+@export var radio_transmitter: radio_transmitter
+var has_power: bool = false
 
 
-func change_electricity(value):
-	print("asd")
-	has_electricity = value
-	if has_electricity:
-		# debugging to find out why audio sometimes fizzles
-		#$".".freeze = true #.linear_velocity = Vector3.ZERO
-		$AudioStreamPlayer3D.play()
+func _ready():
+	if radio_transmitter == null:
+		print("Error: Radio transmitter not set!")
+
+
+func _on_plug_electricity_changed(value):
+	has_power = value
+	if has_power:
+		if radio_transmitter.has_power:
+			$morse.play()
+			$kohina.stop()
+		else:
+			$morse.stop()
+			$kohina.play()
 	else:
-		$AudioStreamPlayer3D.stop()
-		#$".".freeze = false
+		$kohina.stop()
+		$morse.stop()
+
+func radio_transmission_started():
+	_on_plug_electricity_changed(has_power)
+
