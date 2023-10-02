fff5776282025e8f08a3920ed630e6afa028e013
Tatu Heinämäki <tatu.heinamaki@gmail.com>
2023-10-02 15:00:41 +0300

    Fixed my mistake on having the pause menu visible.

--- scripts/Menus and highscore/pause_menu.gd
+++ scripts/Menus and highscore/pause_menu.gd
@@ -1,6 +1,12 @@
 extends PanelContainer
 
 
+# Making sure the pause menu is not visible when game starts
+func _ready():
+	if visible:
+		visible = false
+
+
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		self.visible = not self.visible
