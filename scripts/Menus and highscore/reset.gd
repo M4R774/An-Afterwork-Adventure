9456fec3fc9fed1f5a1656e4e7bc24aa1c6e54f7
Aleksi Martikainen <aleksi.martikainen@gofore.com>
2023-10-02 03:08:09 +0300

    Fix bug, mouse was locked in menu

--- scripts/Menus and highscore/reset.gd
+++ scripts/Menus and highscore/reset.gd
@@ -3,5 +3,6 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	HIGHSCORE_SINGLETON.SCORE = null
 	HIGHSCORE_SINGLETON.START_TIME = null
