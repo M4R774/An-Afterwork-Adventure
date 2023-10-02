9abefa0eb7259c1af78d8b4a3ab652f0389b28ec
Tatu Heinämäki <tatu.heinamaki@gmail.com>
2023-10-02 15:20:13 +0300

    New player character and keyboard start layout. Added morse book for real. Fixed bug that crashed game when picking up keycard.

--- scripts/puzzle 1/door.gd
+++ scripts/puzzle 1/door.gd
@@ -1,8 +1,11 @@
 extends MeshInstance3D
 
+var has_opened = false # hack to not play dooring closing animation at start
 
 func _on_plug_electricity_changed(value):
 	if value:
 		$AnimationPlayer.play("open_door")
+		has_opened = true
 	else:
-		$AnimationPlayer.play_backwards("open_door")
+		if has_opened:
+			$AnimationPlayer.play_backwards("open_door")
